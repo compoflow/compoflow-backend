cccb48fd9daec6c4cf88261d4833f1a7959a7697
lavender-qaq <1254297317@qq.com>
2022-05-09 15:41:41 +0800

    Use kubebuilder to monitor k8s resources

--- README.md
+++
@@ -1,1 +0,0 @@
-# microservice-workflows-backend
--- main.go
+++ main.go
@@ -1,55 +1,138 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
 
 import (
 	"flag"
-	"fmt"
+	"os"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	//+kubebuilder:scaffold:imports
 
 	"github.com/Lavender-QAQ/microservice-workflows-backend/executer/kubernetes"
 	"github.com/Lavender-QAQ/microservice-workflows-backend/handler"
 	"github.com/Lavender-QAQ/microservice-workflows-backend/router"
-	"github.com/go-logr/logr"
-	"github.com/go-logr/zapr"
-	"go.uber.org/zap"
 )
 
-var logger logr.Logger
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+	logger   = ctrl.Log.WithName("run")
+)
 
-func main() {
-	kubeconfigPath := flag.String("kubeconfig", "./kubeconfig", "Kubernetes configuration file location")
-	listen := flag.String("listen", "127.0.0.1:30086", "Specify the listening ip address and port")
-	flag.Parse()
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-	err := registerLogger()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	logger.WithValues("kubeconfig location", *kubeconfigPath).Info("Kubeconfig parameters were successfully parsed")
-
-	err = kubernetes.Init(*kubeconfigPath)
-	if err != nil {
-		logger.Error(err, "Fail to initialize kubernetes cluster")
-		return
-	}
-
-	err = router.NewRouter(*listen)
-	if err != nil {
-		logger.Error(err, "Fail to create router")
-		return
-	}
+	//+kubebuilder:scaffold:scheme
 }
 
 func registerLogger() error {
-	zapLog, err := zap.NewDevelopment()
-	if err != nil {
-		return fmt.Errorf("who watches the watchmen (%v)?", err)
-	}
-	logger = zapr.NewLogger(zapLog)
 
 	// Register handler
 	handler.HandlerLogger = logger.WithName("Handler")
 	router.RouterLogger = logger.WithName("Router")
 
 	return nil
+}
+
+func main() {
+	var metricsAddr string
+	var enableLeaderElection bool
+	var probeAddr string
+	var listen string
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+
+	flag.StringVar(&listen, "listen", "127.0.0.1:30086", "Specify the listening ip address and port")
+
+	opts := zap.Options{
+		Development: true,
+	}
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
+
+	// Get restconfig
+	restConf := ctrl.GetConfigOrDie()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	mgr, err := ctrl.NewManager(restConf, ctrl.Options{
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   9443,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "3826945b.my.domain",
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	// Register logger
+	err = registerLogger()
+	if err != nil {
+		logger.Error(err, "Fail to register logger")
+		return
+	}
+
+	// Init client-go
+	err = kubernetes.Init(restConf)
+	if err != nil {
+		logger.Error(err, "Fail to initialize kubernetes cluster")
+		return
+	}
+
+	// Launch Router
+	go router.NewRouter(listen)
+
+	//+kubebuilder:scaffold:builder
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+
+	if err != nil {
+		setupLog.Error(err, "error getting kubeconfig")
+		os.Exit(1)
+	}
+
 }
